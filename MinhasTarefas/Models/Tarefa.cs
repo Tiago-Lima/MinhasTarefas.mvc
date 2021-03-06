55f252831ddfaa697a9ff2d9e0393af4a240d9d9
Tiago-Lima <tiago.santoslima91@gmail.com>
2021-03-06 16:18:17 -0300

    CommitInicial

---
+++ MinhasTarefas/Models/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefas.Models
+{
+    public class Tarefa
+    {
+       
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Data { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
